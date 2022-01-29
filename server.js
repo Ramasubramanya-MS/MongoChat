3ae24853e88f999b4482f8908393eac23f99779d
Ramasubramanya-MS <81099796+Ramasubramanya-MS@users.noreply.github.com>
2022-01-29 19:53:58 +0530

    Improved Code

--- server.js
+++ server.js
@@ -1,63 +1,64 @@
-const mongo = require('mongodb').MongoClient;
-const client = require('socket.io')(4000).sockets;
+var express = require('express')
+var bodyParser = require('body-parser')
+var app = express()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
+var mongoose = require('mongoose')
 
-// Connect to mongo
-mongo.connect('mongodb://127.0.0.1/mongochat', function(err, db){
-    if(err){
-        throw err;
+app.use(express.static(__dirname))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended : false}))
+
+//DB Connection using the MongoDB 
+//const dbUrl = Removing connect url for GitHub Upload.
+const dbUrl = "mongodb+srv://admin:admin@mongo-node.jeyks.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+var server = http.listen(3001, () => {
+	console.log('Server is listening on port ', server.address().port)
+})
+
+io.on('connection', (socket) => {
+	console.log('User connected..')
+})
+
+mongoose.connect(dbUrl, { useNewUrlParser: true }, (err) =>{
+    console.log('mongo db connection', err)
+})
+
+mongoose.Promise = Promise
+
+
+var Message = mongoose.model('Message', {
+    name: String,
+    message: String
+})
+
+app.get('/messages', (req,res) => {
+    Message.find({}, (err, messages) => {
+        res.send(messages)
+    })
+}) 
+
+app.post('/messages', async (req, res) => {
+
+    try {
+        var message = new Message(req.body)
+        var savedMessage = await message.save()
+        console.log('saved', savedMessage)
+
+        var censored = await Message.findOne({ message: 'badword' })
+
+        if (censored)
+            await Message.remove({ _id: censored.id })
+        else
+            io.emit('message', req.body)
+
+        res.sendStatus(200)
+    } catch (error) {
+        res.sendStatus(500)
+        return console.error(error)
+    } finally {
+        console.log('message post called')
     }
+})
 
-    console.log('MongoDB connected...');
-
-    // Connect to Socket.io
-    client.on('connection', function(socket){
-        let chat = db.collection('chats');
-
-        // Create function to send status
-        sendStatus = function(s){
-            socket.emit('status', s);
-        }
-
-        // Get chats from mongo collection
-        chat.find().limit(100).sort({_id:1}).toArray(function(err, res){
-            if(err){
-                throw err;
-            }
-
-            // Emit the messages
-            socket.emit('output', res);
-        });
-
-        // Handle input events
-        socket.on('input', function(data){
-            let name = data.name;
-            let message = data.message;
-
-            // Check for name and message
-            if(name == '' || message == ''){
-                // Send error status
-                sendStatus('Please enter a name and message');
-            } else {
-                // Insert message
-                chat.insert({name: name, message: message}, function(){
-                    client.emit('output', [data]);
-
-                    // Send status object
-                    sendStatus({
-                        message: 'Message sent',
-                        clear: true
-                    });
-                });
-            }
-        });
-
-        // Handle clear
-        socket.on('clear', function(data){
-            // Remove all chats from collection
-            chat.remove({}, function(){
-                // Emit cleared
-                socket.emit('cleared');
-            });
-        });
-    });
-});
